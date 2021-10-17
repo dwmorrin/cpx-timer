8e58e4ab810c2448957933d8123ee2170a6dbe2f
David Morrin <dwmorrin@gmail.com>
2021-10-16 21:49:02 -0400

    feat: adds README

---
+++ README.md
@@ -0,0 +1,6 @@
+Using the Circuit Playground Express as a timer that plays a melody while the timer is running.
+NeoPixels animated in time with the melody.
+Button A starts the timer and button B can cancel the timer.
+
+Using Arduino rather than MakeCode or CircuitPython for more control over the programming the melody
+and lights.
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 #define BUTTON_A 4
 #define BUTTON_B 5
 
-enum DurationMS {
+enum DurationMS
+{
   sixteenth = 125,
   eighth = 250,
   eighth_triplet = 333,
@@ -16,7 +17,8 @@ enum DurationMS {
   half = 1000,
 };
 
-enum Interval {
+enum Interval
+{
   rest = -255,
   unison = 0,
   m2,
@@ -33,110 +35,120 @@ enum Interval {
   octave,
 };
 
-struct Note {
+struct Note
+{
   int interval;
   int duration;
 };
 
 const int MELODY_LENGTH = 51;
 struct Note melody[] = {
-  {P5, quarter},
-  {unison, dotted_quarter},
-  {unison, eighth},
-  {unison, sixteenth},
-  {M2, sixteenth},
-  {M3, sixteenth},
-  {P4, sixteenth},
-  {P5, half},
-  {rest, eighth},
-  {P5, eighth},
-  {P5, eighth_triplet},
-  {m6, eighth_triplet},
-  {m7, eighth_triplet},
-  {octave, half},
-  {rest, eighth},
-  {octave, eighth},
-  {octave, sixteenth},
-  {m7, sixteenth},
-  {m6, eighth},
-  {m7, dotted_eighth},
-  {m6, sixteenth},
-  {P5, half},
-  {P5, quarter},
-  {P4, eighth},
-  {P4, sixteenth},
-  {P5, sixteenth},
-  {m6, half},
-  {P5, eighth},
-  {P4, eighth},
-  {m3, eighth},
-  {m3, sixteenth},
-  {P4, sixteenth},
-  {P5, half},
-  {P4, eighth},
-  {m3, eighth},
-  {M2, eighth},
-  {M2, sixteenth},
-  {M3, sixteenth},
-  {aug4, half},
-  {M6, quarter},
-  {P5, eighth},
-  {P5, sixteenth},
-  {P5, sixteenth},
-  {P5, eighth},
-  {P5, sixteenth},
-  {P5, sixteenth},
-  {P5, eighth},
-  {P5, sixteenth},
-  {P5, sixteenth},
-  {P5, eighth},
-  {P5, eighth},
+    {P5, quarter},
+    {unison, dotted_quarter},
+    {unison, eighth},
+    {unison, sixteenth},
+    {M2, sixteenth},
+    {M3, sixteenth},
+    {P4, sixteenth},
+    {P5, half},
+    {rest, eighth},
+    {P5, eighth},
+    {P5, eighth_triplet},
+    {m6, eighth_triplet},
+    {m7, eighth_triplet},
+    {octave, half},
+    {rest, eighth},
+    {octave, eighth},
+    {octave, sixteenth},
+    {m7, sixteenth},
+    {m6, eighth},
+    {m7, dotted_eighth},
+    {m6, sixteenth},
+    {P5, half},
+    {P5, quarter},
+    {P4, eighth},
+    {P4, sixteenth},
+    {P5, sixteenth},
+    {m6, half},
+    {P5, eighth},
+    {P4, eighth},
+    {m3, eighth},
+    {m3, sixteenth},
+    {P4, sixteenth},
+    {P5, half},
+    {P4, eighth},
+    {m3, eighth},
+    {M2, eighth},
+    {M2, sixteenth},
+    {M3, sixteenth},
+    {aug4, half},
+    {M6, quarter},
+    {P5, eighth},
+    {P5, sixteenth},
+    {P5, sixteenth},
+    {P5, eighth},
+    {P5, sixteenth},
+    {P5, sixteenth},
+    {P5, eighth},
+    {P5, sixteenth},
+    {P5, sixteenth},
+    {P5, eighth},
+    {P5, eighth},
 };
 
 // helper function to map integers to musical frequencies
-uint32_t n2freq(uint8_t n) {
+uint32_t n2freq(uint8_t n)
+{
   // leading constant is the base frequency of which n is n steps away from result
-  return uint32_t(220.0 * pow(2, n/12.0));
+  return uint32_t(220.0 * pow(2, n / 12.0));
 }
 
 // global NeoPixel object to share across program
 static Adafruit_NeoPixel *pixels;
 
-uint32_t note2rgb(struct Note note) {
+uint32_t note2rgb(struct Note note)
+{
   uint8_t r = map(note.interval, 0, 12, 170, 0);
   uint8_t g = map(note.interval, 0, 12, 30, 170);
   uint8_t b = map(note.duration, 125, 1000, 30, 170);
   return pixels->Color(r, g, b);
 }
 
-uint16_t _random_pixel() {
+uint16_t _random_pixel()
+{
   return random(0, N_PIXELS);
 }
 
-uint16_t random_pixel() {
+uint16_t random_pixel()
+{
   static uint16_t last_pixel = _random_pixel();
   uint16_t next_pixel = _random_pixel();
-  while (next_pixel == last_pixel) {
+  while (next_pixel == last_pixel)
+  {
     next_pixel = _random_pixel();
   }
   last_pixel = next_pixel;
   return next_pixel;
 }
 
-void play_melody_for_n_seconds(uint32_t s) {
+void play_melody_for_n_seconds(uint32_t s)
+{
   uint32_t ms = s * 1000;
   uint32_t start = millis();
   uint8_t melody_index = 0;
   // melody stops after s seconds or button B is pressed
-  while (millis() - start < ms && !digitalRead(BUTTON_B)) {
+  while (millis() - start < ms && !digitalRead(BUTTON_B))
+  {
     struct Note note = melody[melody_index];
     melody_index = (melody_index + 1) % MELODY_LENGTH;
     pixels->clear();
-    if (note.interval > rest) {
+    if (note.interval > rest)
+    {
       pixels->setPixelColor(random_pixel(), note2rgb(note));
       tone(PIN_A0, n2freq(note.interval), note.duration);
     }
-    else {
+    else
+    {
       noTone(PIN_A0);
     }
     pixels->show();
@@ -147,11 +159,13 @@ void play_melody_for_n_seconds(uint32_t s) {
   pixels->show();
 }
 
-void play_melody_for_n_minutes(uint32_t m) {
+void play_melody_for_n_minutes(uint32_t m)
+{
   play_melody_for_n_seconds(m * 60);
 }
 
-void setup() {
+void setup()
+{
   pinMode(BUTTON_A, INPUT);
   pinMode(BUTTON_B, INPUT);
   pixels = new Adafruit_NeoPixel(N_PIXELS, PIN_NEO, NEO_GRB + NEO_KHZ800);
@@ -160,12 +174,15 @@ void setup() {
   pixels->show();
 }
 
-void loop() {
+void loop()
+{
   // debounce button A and play melody if pressed
   static int button_a_count = 0;
-  if (digitalRead(BUTTON_A)) {
+  if (digitalRead(BUTTON_A))
+  {
     ++button_a_count;
-    if (button_a_count > 10) {
+    if (button_a_count > 10)
+    {
       button_a_count = 0;
       play_melody_for_n_minutes(2);
     }
